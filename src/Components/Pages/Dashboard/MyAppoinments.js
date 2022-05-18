4b520ca5915831990e8553c242bafd79efa93f99
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-05-18 20:11:13 +0600

    get data using jwt verification

--- src/Components/Pages/Dashboard/MyAppoinments.js
+++ src/Components/Pages/Dashboard/MyAppoinments.js
@@ -1,18 +1,43 @@
+import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import auth from "../../../firebase.init";
 
 const MyAppoinments = () => {
     const [treatment, setTreatment] = useState([]);
     const [user] = useAuthState(auth);
+    const navigate = useNavigate()
 
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/treatment?patient=${user.email}`)
-                .then((res) => res.json())
+            fetch(`http://localhost:5000/treatment?patient=${user.email}`, {
+                method: "GET",
+                headers: {
+                    'authorization' : `Bearer ${localStorage.getItem(
+                        "accessToken"
+                    )}`,
+                },
+            })
+                .then((res) => {
+                    if(res.status === 401){
+                        signOut(auth);
+                        navigate('/')
+                    }
+                    else if (res.status === 403){
+                        toast.error("You have no access permmision")
+                    }
+                    else{
+                        return res.json();
+                    }
+                    
+                   
+                })
                 .then((data) => {
-                    setTreatment(data);
+                    
                     console.log(data);
+                    setTreatment(data);
                 });
         }
     }, [user]);
@@ -29,7 +54,7 @@ const MyAppoinments = () => {
                         </tr>
                     </thead>
                     <tbody className="text-center">
-                        {treatment.map((t, index) => (
+                        {treatment?.map((t, index) => (
                             <tr>
                                 <th>{index + 1}</th>
                                 <td>{t.patientName}</td>
