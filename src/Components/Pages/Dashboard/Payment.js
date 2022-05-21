c185914209b6518fe3e660082664b8cc357694a3
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-05-21 11:54:48 +0600

    added a stipe payment method

--- src/Components/Pages/Dashboard/Payment.js
+++ src/Components/Pages/Dashboard/Payment.js
@@ -1,12 +1,19 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React, { useEffect } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import Loading from "../../Shared/Loading";
+import CheckOutForm from "./CheckOutForm";
+
+const stripePromise = loadStripe(
+    "pk_test_51L1O6gI47NxtigIiVGGa6N58wX0VUowpwuRXE3omGHjeO52ZKFDFGDlbwECCAO4DpajaCMxEYsIyhsbK8YY3bRQc00siTVLMiA"
+);
 
 const Payment = () => {
     const { id } = useParams();
     const url = `https://desolate-castle-78820.herokuapp.com/booking/${id}`;
-    const { data, isLoading } = useQuery(["booking", id], () =>
+    const { data: appoinment, isLoading } = useQuery(["booking", id], () =>
         fetch(url, {
             method: "get",
             headers: {
@@ -23,21 +30,20 @@ const Payment = () => {
             <div class="card lg:max-w-lg bg-base-100 shadow-xl">
                 <div class="p-6 text-left">
                     <h2 class="card-title text-secondary">
-                        Hello, {data.patientName}
+                        Hello, {appoinment.patientName}
                     </h2>
                     <p>
-                        Your Appoinment in {data.date} at {data.slot}
+                        Your Appoinment in {appoinment.date} at{" "}
+                        {appoinment.slot}
                     </p>
-                    <p>Please Pay ${data.Price}</p>
+                    <p>Please Pay ${appoinment.Price}</p>
                 </div>
             </div>
             <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
+                <div class="p-6 w-96">
+                    <Elements stripe={stripePromise}>
+                        <CheckOutForm appoinment={appoinment} />
+                    </Elements>
                 </div>
             </div>
         </div>
